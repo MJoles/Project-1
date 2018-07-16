f9b6cbf49b827576877b4089a4746411c6e27447
Torrenceh98 <torrenceh98@gmail.com>
2018-07-16 18:51:21 -0500

    share

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,3 +1,65 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+ }
+ 
+ function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+ }
+ function openSearch() {
+     document.getElementById("myOverlay").style.display = "block";
+ }
+ 
+ function closeSearch() {
+     document.getElementById("myOverlay").style.display = "none";
+ }
+
+ $("#submitBtn").on("click", function(event){
+    event.preventDefault();
+    
+    var artistSearch = $("#inputSearch").val();
+
+    var searchMe = artistSearch;
+    
+    console.log(artistSearch);
+  
+    
+
+     console.log()
+
+
+var queryURL = "http://api.genius.com/search?q=" + artistSearch + "&access_token=sGRyLhVTDxgaowHGUqtUkREYFGH5Tdz6STVnF1pByg2JwCoRaTmzkw9iHVC2lS_c" ;
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+
+    var ourLyrics = response.response.hits;
+
+  for (var i = 0; i < ourLyrics.length; i++) {
+
+var realLyrics = response.response.hits[i].result.url
+
+realLyrics 
+    console.log(response.response.hits[i]);
+
+$("#lyrics").append(response.response.hits[i].result.url)
+
+  }
+  
+
+
+
+    
+
+
+});
+
+
+
+
+ 
+
 $(document).ready(function(){
 	$('a[href="#search"]').on('click', function(event) {                    
 		$('#search').addClass('open');
@@ -9,6 +71,7 @@ $(document).ready(function(){
 		}
 	});            
 });
+});
 
 $("#search-nav").on('click', function(){
 
@@ -19,15 +82,7 @@ openSearch() ;
 
 
 
-// var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
-
-// $.ajax({
- // url: queryURL,
-  //method: "GET"
-//}).then(function(response) {
- // console.log(response);
-//});
 
 
- //"http://api.genius.com/search?q=" + Kendrick%20Lamar + "&access_token=sGRyLhVTDxgaowHGUqtUkREYFGH5Tdz6STVnF1pByg2JwCoRaTmzkw9iHVC2lS_c" ;
+ 
 
