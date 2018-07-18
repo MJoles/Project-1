e436d20f5da2cdf6fb3daae1fff827135192ff12
Torrenceh98 <torrenceh98@gmail.com>
2018-07-18 18:59:07 -0500

    btns

--- assets/javascript/project.js
+++ assets/javascript/project.js
@@ -1,3 +1,19 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAVAgMQ160Gjz9cwBkiiXtlXHvEQwX-n8U",
+    authDomain: "groupproject1-bf127.firebaseapp.com",
+    databaseURL: "https://groupproject1-bf127.firebaseio.com",
+    projectId: "groupproject1-bf127",
+    storageBucket: "",
+      };
+  firebase.initializeApp(config);
+ 
+  var dataRef = firebase.database();
+
+
+$("#lyricBtn").hide()
+$("#list").hide()
+
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
  }
@@ -18,66 +34,56 @@ function openNav() {
     
     var artistSearch = $("#inputSearch").val();
 
-    var searchMe = artistSearch;
+   //Creates variable for holding data
+   var newArtistSearch = {
+    artist: artistSearch
+};
+
+//Uploads artist search to the database
+dataRef.ref().push(newArtistSearch);
     
     console.log(artistSearch);
-  
-    
-
-     console.log()
 
 
-var queryURL = "http://api.genius.com/search?q=" + artistSearch + "&access_token=sGRyLhVTDxgaowHGUqtUkREYFGH5Tdz6STVnF1pByg2JwCoRaTmzkw9iHVC2lS_c" ;
+var queryURL = "https://api.audd.io/?method=findLyrics&q=" + artistSearch +  "&api_token=f846db22eaf81cb3e7a79d0af9df399c"
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
 
-    var ourLyrics = response.response.hits;
-
-  for (var i = 0; i < ourLyrics.length; i++) {
-
-var realLyrics = response.response.hits[i].result.url
-
-realLyrics 
-    console.log(response.response.hits[i]);
-
-$("#lyrics").append(response.response.hits[i].result.url)
-
-  }
-  
-
-
-
+    
+console.log(response.result[0].lyrics);
     
 
+for (var i = 0; i < response.result.length; i++) {
 
+var ourLyrics = response.result[i].lyrics;
+var ourSongs = response.result[i].full_title;
+
+
+
+
+var lyricBtn = $('<button id="lyricBtn" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong'+ [i] + '" > Lyrics </button>')
+
+var modalBtn = $('<div class="modal fade" id="exampleModalLong'+ [i] + '" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true"> <div class="modal-dialog" role="document"> <div class="modal-content"> <div class="modal-header"> <h5 class="modal-title" id="exampleModalLongTitle">LYRICS</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">&times;</span> </button> </div> <div id="insideBtn' + [i] + '" class="modal-body">' + ourLyrics + '</div> <div class="modal-footer"> <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> ')
+
+$("#songs").append('<br/>' + ourSongs + "<br/>")
+$("#songs").append(modalBtn)
+$("#songs").append(lyricBtn)
+
+
+  }  
 });
 
+
+$("#lyricBtn").show()
 
 
 
  
 
-$(document).ready(function(){
-	$('a[href="#search"]').on('click', function(event) {                    
-		$('#search').addClass('open');
-		$('#search > form > input[type="search"]').focus();
-	});            
-	$('#search, #search button.close').on('click keyup', function(event) {
-		if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-			$(this).removeClass('open');
-		}
-	});            
 });
-});
-
-$("#search-nav").on('click', function(){
-
-openSearch() ;
-
-})
 
 
 
